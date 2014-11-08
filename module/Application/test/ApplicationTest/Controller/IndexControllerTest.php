edccab2e4863126cb5d9b48c338c017d69e3aeb5
Ross Basarevych <basarevych@gmail.com>
2014-11-08 15:57:45 +0200

    db-data tests

--- module/Application/test/ApplicationTest/Controller/IndexControllerTest.php
+++ module/Application/test/ApplicationTest/Controller/IndexControllerTest.php
@@ -23,4 +23,23 @@ class IndexControllerTest extends AbstractHttpControllerTestCase
         $this->assertControllerClass('IndexController');
         $this->assertMatchedRouteName('application');
     }
+
+    public function testDbDataActionCanBeAccessed()
+    {
+        $this->dispatch('/index/db-data?query=discover');
+        $this->assertResponseStatusCode(200);
+
+        $this->assertModuleName('application');
+        $this->assertControllerName('application\controller\index');
+        $this->assertControllerClass('IndexController');
+        $this->assertMatchedRouteName('application');
+
+        $this->dispatch('/index/db-data?query=data');
+        $this->assertResponseStatusCode(200);
+
+        $this->assertModuleName('application');
+        $this->assertControllerName('application\controller\index');
+        $this->assertControllerClass('IndexController');
+        $this->assertMatchedRouteName('application');
+    }
 }
