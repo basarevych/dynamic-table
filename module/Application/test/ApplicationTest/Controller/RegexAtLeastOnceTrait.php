6c2aba3b99a9353c6e9a7e2e26c33e8282802ee0
Ross Basarevych <basarevych@gmail.com>
2015-03-03 05:54:23 +0200

    RegexAtLeastOnce trait

---
+++ module/Application/test/ApplicationTest/Controller/RegexAtLeastOnceTrait.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ApplicationTest\Controller;
+
+use Zend\Dom\Document;
+use PHPUnit_Framework_ExpectationFailedException;
+
+trait RegexAtLeastOnceTrait
+{
+    private function assertQueryContentRegexAtLeastOnce($path, $pattern, $useXpath = false)
+    {
+        $response = $this->getResponse();
+        $document = new Document($response->getContent());
+
+        if ($useXpath) {
+            $document->registerXpathNamespaces($this->xpathNamespaces);
+        }
+
+        $result   = Document\Query::execute($path, $document, $useXpath ? Document\Query::TYPE_XPATH : Document\Query::TYPE_CSS);
+
+        if ($result->count() == 0) {
+            throw new PHPUnit_Framework_ExpectationFailedException(sprintf(
+                'Failed asserting node DENOTED BY %s EXISTS',
+                $path
+            ));
+        }
+
+        foreach ($result as $node) {
+            if (preg_match($pattern, $node->nodeValue))
+                return;
+        }
+
+        throw new PHPUnit_Framework_ExpectationFailedException(sprintf(
+            'Failed asserting node denoted by %s CONTAINS content MATCHING "%s"',
+            $path,
+            $pattern
+        ));
+    }
+}
